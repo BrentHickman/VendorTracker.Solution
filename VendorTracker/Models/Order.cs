21693fc55b2c5b12c9e62253288807cc8b6e07d0
BrentHickman <brenthickman@ymail.com>
2023-03-03 11:15:42 -0800

    Order passed 6th test.

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -5,29 +5,26 @@ namespace VendorTracker.Models
   public class Order
   {
     public string Title { get; set; }
-    // public int Id { get; }
+    public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
-
+    
     public Order(string title)
     {
       Title = title;
       _instances.Add(this);
-      // Id = _instances.Count;
+      Id = _instances.Count;
     }
-
     public static List<Order> GetAll()
     {
       return _instances;
     }
-
     public static void ClearAll()
     {
       _instances.Clear();
     }
-
-    // public static Order Find(int searchId)
-    // {
-    //   return _instances[searchId-1];
-    // }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
 }
